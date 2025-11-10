c5b5c27b0e1b4dcd4f7f93fea528d8bc9c963cf3
oyeleen <oyeleen@proton.me>
2025-11-10 17:02:55 +0300

    feat: encoding line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i + 1]) {
+      count++;
+    }
+    else {
+      result += ((count > 1) ? count : '') + str[i];
+      count = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = {
