978aa61c2ccded6727ebe814cf382e14cd1d39b5
oyeleen <oyeleen@proton.me>
2025-11-10 16:48:22 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) == 'string' && Number(sampleActivity) && sampleActivity < 15 && sampleActivity > 0)
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  else
+    return false;
 }
 
 module.exports = {
