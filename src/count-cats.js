7a23fd7a6a5366246492c338eacf2ba52ed51562
oyeleen <oyeleen@proton.me>
2025-11-10 16:50:29 +0300

    feat: count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats( matrix ) {
+  let count = 0;
+  for (let row of matrix)
+    for (let cat of row)
+      if (cat == '^^')
+        count++;
+
+  return count;
 }
 
 module.exports = {
