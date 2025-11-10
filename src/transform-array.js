9883569fe611087cc578069c58859dcf985c721c
oyeleen <oyeleen@proton.me>
2025-11-10 18:31:32 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,33 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  let new_arr = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i = (arr[i + 2] == '--double-prev' || arr[i + 2] == '--discard-prev') ? i + 2 : i + 1;
+        break;
+      case '--discard-prev':
+        if (new_arr.length) new_arr.pop();
+        break;
+      case '--double-next':
+        if (arr[i + 1]) {
+          new_arr.push(arr[i + 1]);
+          new_arr.push(arr[i + 1]);
+          i++;
+        }
+        break;
+      case '--double-prev':
+        if (new_arr.length) new_arr.push(new_arr[new_arr.length - 1]);
+        break;
+      default:
+        new_arr.push(arr[i]);
+        break;
+    }
+  }
+  return new_arr;
 }
 
 module.exports = {
